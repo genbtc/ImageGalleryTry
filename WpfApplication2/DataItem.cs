8a6312c1c6e68a0a027a4984e7127604abc84e6e
gen BTC <genBTC@hushmail.com>
2014-09-17 10:52:47 -0400

    Image gallery Browser

---
+++ WpfApplication2/DataItem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GalleryTry
+{
+    public class DataItem
+    {
+        private readonly string _title;
+        private readonly Uri _picture;
+
+        public string Title
+        {
+            get
+            {
+                return _title;
+            }
+        }
+
+        public Uri Picture
+        {
+            get
+            {
+                return _picture;
+            }
+        }
+
+        //constructor without title.
+        public DataItem(string picture)
+        {
+            //_title = title;
+            _picture = new Uri(picture, UriKind.Absolute);
+        }
+        
+        public DataItem(string title, string picture)
+        {
+            _title = title;
+            _picture = new Uri(picture, UriKind.Absolute);
+        }
+    }
+}
---
+++ WpfApplication2/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using MoreLinq;
+
+namespace GalleryTry
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public IEnumerable<List<DataItem>> Images { get; private set; }
+
+        public ViewModel()
+        {
+            var photospathlist = Directory.EnumerateFiles(@"C:\Users\EOFL\Pictures", "*.jpg", SearchOption.AllDirectories); 
+
+            IEnumerable<DataItem> dataitemlist = photospathlist.Select(file => new DataItem(file));
+
+            Images = dataitemlist.Batch(3).Select(x => x.ToList());
+            RaisePropertyChanged("Images");
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void RaisePropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
+    }
+}
