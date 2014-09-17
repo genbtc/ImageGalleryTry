cf5f61ccefadd0a240a9b24214da77df681369a1
gen BTC <genBTC@hushmail.com>
2014-09-17 11:42:34 -0400

    Try both side by side

--- WpfApplication2/ViewModel.cs
+++ WpfApplication2/ViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Linq;
@@ -8,6 +9,7 @@ namespace GalleryTry
 {
     public class ViewModel : INotifyPropertyChanged
     {
+        public IEnumerable<List<DataItem>> Images2 { get; private set; }
         public IEnumerable<List<DataItem>> Images { get; private set; }
 
         public ViewModel()
@@ -15,9 +17,13 @@ namespace GalleryTry
             var photospathlist = Directory.EnumerateFiles(@"C:\Users\EOFL\Pictures", "*.jpg", SearchOption.AllDirectories); 
 
             IEnumerable<DataItem> dataitemlist = photospathlist.Select(file => new DataItem(file));
+            IEnumerable<DataItem> dataItems = dataitemlist as DataItem[] ?? dataitemlist.ToArray();
 
-            Images = dataitemlist.Batch(3).Select(x => x.ToList());
+            
+            Images = dataItems.Batch(3).Select(x => x.ToList());
             RaisePropertyChanged("Images");
+            Images2 = dataItems.Batch(1).Select(x => x.ToList());
+            RaisePropertyChanged("Images2");
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
